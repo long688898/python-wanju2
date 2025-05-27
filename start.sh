fb0167a82d14e9c5b9c00cd9c281488200a91dbf
Li Lei <173596388+long688898@users.noreply.github.com>
2025-05-27 19:02:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # //隧道相关设置（去掉下面变量前面#启用，否则使用临时隧道）
-export TOK=${TOK:-'eyJhIjoiZWU4NWQ5YmQwNTI0OThhNmIwNWY0N2NjZmQ1YjM1MzIiLCJ0IjoiMjA3ZmZjNTUtZjFjYS00Y2YyLTgzN2YtOGExYWE5ZjA4YTVkIiwicyI6Ik5qa3hNekJsWVRJdE9HUXpNeTAwTWpSaUxUbGxNVFF0Wm1RMk0yRTBabUV5TVRnMCJ9'}  # 隧道token或json
+export TOK=${TOK:-'eyJhIjoiZWU4NWQ5YmQwNTI0OThhNmIwNWY0N2NjZmQ1YjM1MzIiLCJ0IjoiYjViZDhiZTItYTA4Zi00Y2MyLTg2ZmEtYWJmYzMyNjAyMmY5IiwicyI6Ik1HWXhObVJsWVRndE9URTNNQzAwWlRjd0xUaGxZVGd0TVRVNVl6Y3dZelExTW1WayJ9'}  # 隧道token或json
 export ARGO_DOMAIN=${ARGO_DOMAIN:-'tong.ab66.filegear-sg.me'} # 隧道域名
 
 # //哪吒相关设置
